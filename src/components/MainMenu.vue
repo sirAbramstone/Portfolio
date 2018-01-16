d4a918d8461f47cda2502fa0ef60ea12b343e9cf
Indrick <wiskonsin1848@gmail.com>
2018-01-16 20:39:53 +0300

    six

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -11,14 +11,19 @@
           </a>
         </li>
       </nav>
+      <nav class="social">
+      <li v-for="(icon, index) of icons">
+        <a v-bind:href="icon">
+          <i v-bind:class="[fab,
+          fabVar[index]]"></i>
+        </a>
+      </li>
+    </nav>
     </div>
   </div>
+ 
 </template>
-<!--<li><a href="https://www.google.ru/"><span class="lnr lnr-home icon"></span></a></li>-->
-<!--<li><a href="https://www.google.ru/"><span class="lnr lnr-layers icon"></span></a></li>-->
-<!--<li><a href="https://www.google.ru/"><span class="lnr lnr-code icon"></span></a></li>-->
-<!--<li><a href="https://www.google.ru/"><span class="lnr lnr-user icon"></span></a></li>-->
-<!--<li><a href="https://www.google.ru/"><span class="lnr lnr-envelope icon"></span></a></li>-->
+
 <script>
   export default {
     data () {
@@ -38,6 +43,19 @@
           'lnr-code',
           'lnr-user',
           'lnr-envelope'
+        ],
+        icons: [
+          'www.google.com',
+          'www.yandex.ru',
+          'www.mail.ru',
+          'www.facebook.com'
+        ],
+        fab: 'fab',
+        fabVar: [
+          'fa-git',
+          'fa-skype',
+          'fa-vk',
+          'fa-telegram-plane'
         ]
       }
     }
@@ -48,12 +66,16 @@
   a {
     text-decoration: none;
   }
+
   .wrap-menu {
     width: 60px;
     background-color: #2D3B40;
     height: 100vh;
     display: flex;
+    flex-direction: column;
+    justify-content: space-between;
   }
+
   .navigation {
     display: flex;
     flex-direction: column;
@@ -69,10 +91,35 @@
     }
   }
 
+  .logo {
+    width: 100%;
+    height: 80px;
+    background-color: black;
+  }
+
   .icon {
     color: #86B0BF;
     font-weight: 100;
     font-size: 30px;
+  }
+
+  .social {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding-bottom: 10px;
+
+    & li {
+      font-size: 1.25em;
+      opacity: .6;
+      margin-top: 5px;
+
+      & a {
+      color: #86B0BF;
+      }
+    } 
   }
 
 </style>
