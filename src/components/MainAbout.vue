cec2d64ba85ada70afead3182a30713bfa273dc3
Danil Abramov <wiskonsin1848@gmail.com>
2018-01-24 19:04:48 +0300

    about styles + animation

--- src/components/MainAbout.vue
+++ src/components/MainAbout.vue
@@ -3,12 +3,8 @@
 	<div class="wrap_about">
 		<main-menu></main-menu>
 		<div class="about_content">
-			<div class="about_content_h">About me</div>
-			<div class="about_content_txt">
-				<p>Lorem ipsum dolor sit amet, eam stet elit explicari in, ei sit aperiam expetenda. Laboramus appellantur contentiones pro ut, ad mentitum laboramus elaboraret cum.</p>
-				<p>Habeo timeam scribentur at pri, eam solum impedit et. Ea vidisse minimum nec, ei causae scribentur mel, quo dictas vituperatoribus an. Per ea erant munere assueverit.</p>
-				<p>Cu saepe euripidis cum, eum suas postulant consectetuer ea, id mea facilis accusata. At est verterem dissentias, adhuc error posidonium quo cu.</p>
-				<p>Pri ea nostro denique luptatum, saperet recteque ea sea. Sea ex percipit reformidans, eu quidam quodsi tincidunt eam. Ex per ferri pericula.</p>
+			<div class="about_content_h" v-html="charSpan(textH)"></div>
+			<div class="about_content_txt" v-for="text in texts" v-html="wordSpan(text)">
 			</div>
 		</div>
 	</div>
@@ -19,9 +15,30 @@
 
 	export default {
 		name: 'About',
+    data () {
+		  return {
+		    textH: 'About me',
+        texts: [
+          'Lorem ipsum dolor sit amet, eam stet elit explicari in, ei sit aperiam expetenda. Laboramus appellantur contentiones pro ut, ad mentitum laboramus elaboraret cum.',
+          'Habeo timeam scribentur at pri, eam solum impedit et. Ea vidisse minimum nec, ei causae scribentur mel, quo dictas vituperatoribus an. Per ea erant munere assueverit.',
+          'Cu saepe euripidis cum, eum suas postulant consectetuer ea, id mea facilis accusata. At est verterem dissentias, adhuc error posidonium quo cu.',
+          'Pri ea nostro denique luptatum, saperet recteque ea sea. Sea ex percipit reformidans, eu quidam quodsi tincidunt eam. Ex per ferri pericula.'
+        ]
+      }
+    },
 		components: {
 			'main-menu': MainMenu
-		}
+		},
+    methods: {
+      wordSpan: function (text) {
+        return text.split(' ').map(function (s) {
+          return '<div class = "word">' + s + '</div>'}).join(' ');
+      },
+      charSpan: function (textH) {
+        return textH.split('').map(function (s) {
+        return '<div class = "char">' + s + '</div>'}).join('');
+      }
+    }
 	}
 </script>
 
@@ -44,15 +61,43 @@
 				@include content-h2;
 				width: 38%;
 				margin-bottom: 20px;
+
+        .char {
+          display: inline-block;
+          transition: transform 1s ease-in-out;
+          font-family:'Raleway';
+
+          &:nth-child(6) {
+            padding-left: 15px;
+          }
+
+          &:hover {
+            animation-name: rubberBand;
+            animation-duration: 1s;
+            animation-fill-mode: both;
+            color: $acid;
+          }
+        }
 			}
 
 			.about_content_txt {
 				@include content-txt;
+        margin-top: 10px;
+        margin-bottom: 10px;
 				width: 45%;
 
-				p {
-					margin: 10px 0 10px;
-				}
+				.word {
+          display: inline-block;
+          transition: transform 1s ease-in-out;
+          color: $white;
+
+          &:hover {
+            animation-name: rubberBand;
+            animation-duration: 1s;
+            animation-fill-mode: both;
+            color: $white;
+          }
+        }
 			}
 		}
 	}
