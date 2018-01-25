0a6bf451816c60bd9114aa96d3c7bdce563a0692
Igirma <mikhail.tsai@gmail.com>
2018-01-26 01:30:24 +0300

    Added: - jQuery; - Blast.js; - Animate.css;

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import Router from 'vue-router';
 import router from './router/router';
+import './assets/blast';
 
 Vue.use(Router);
+
+
 
 new Vue({
   el: '#app',
