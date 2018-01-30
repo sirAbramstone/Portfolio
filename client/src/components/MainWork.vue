eb436fa4c2d566cdf3b43261d5cfef1c2ab7ab9b
Сигитов Дмитрий <golovsha@gmail.com>
2018-01-30 16:00:48 +0300

    сделал mainWorks, немного там, немного сям

---
+++ client/src/components/MainWork.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="wrap_work">
+    <main-menu></main-menu>
+    <div id="demo" class="container gallery magicwall">
+      <ul class="magicwall-grid">dental group
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+        <li
+          data-thumb="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550">
+          <div class="hover-content vcenter"></div>
+          <a
+            href="https://i2.wp.com/www.curvyhipsandtintedlips.com/wp-content/uploads/2016/11/b9fc8783bfad11b503088863f56b1d2a.jpg?resize=550%2C550"></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  import MainMenu from './MainMenu'
+
+
+  export default {
+    name: 'Work',
+
+
+
+    components: {
+      'main-menu': MainMenu
+    },
+    mounted() {
+        $(".magicwall").magicWall({
+          maxItemHeight: 300,
+          maxItemWidth: 300,
+          delay: 400,
+          loadingMode:"chain",
+          preloadBeforeSwitch: true,
+          pauseOnHover: "item",
+          animations: "flipY,rollOutX,-rollOutX,rollOutY,-rollOutY,slideColumn,-slideColumn,slideRow,-slideRow,fade",
+          duration:800
+
+        });
+
+        $(".colorbox").colorbox({
+          maxWidth:"90%",
+          maxHeight:"90%",
+          onOpen: function(){
+            $(".magicwall").magicWall("stop");
+          },
+
+          onClosed: function(){
+            $(".magicwall").magicWall("start");
+          }
+        });
+
+      }
+  };
+</script>
+
+<style lang="scss">
+  @import "../styles/main";
+  @import "../styles/third-party";
+
+  .wrap_work{
+    background: $gradient;
+    display: flex;
+
+      #demo{
+        width: 100%;
+      }
+  }
+</style>
--- client/src/router/router.js
+++ client/src/router/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import MainHome from '../components/MainHome';
 import MainSkills from '../components/MainSkills';
 import MainAbout from '../components/MainAbout';
+import MainWork from '../components/MainWork';
 
 Vue.use(Router);
 
@@ -23,6 +24,12 @@ export default new Router({
       path: '/about',
       name: 'About',
       component: MainAbout
+    },
+    {
+      path: '/work',
+      name: 'Work',
+      component: MainWork
     }
   ]
 })
+
