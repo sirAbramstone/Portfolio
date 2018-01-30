504b08c2cc60189ea98d2de737d9356cfefb35b3
Сигитов Дмитрий <golovsha@gmail.com>
2018-01-30 17:45:02 +0300

    ready contact

---
+++ client/src/components/MainContact.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrap_contact">
+    <main-menu></main-menu>
+  </div>
+</template>
+
+<script>
+  import MainMenu from './MainMenu';
+
+  export default {
+    name: 'Contact',
+    components: {
+      'main-menu': MainMenu
+    }
+  }
+</script>
